f61ad6831d337bb0a11b3b0d3a0059ab198f9774
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-12-18 14:40:18 -0300

    localStorage

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,13 @@
 const stars = document.querySelectorAll('.estrelas > i');
+const local = localStorage.getItem('estrelas-sportv') || localStorage.setItem('estrelas-sportv', 1)
 let atual = 1;
 
 stars.forEach((star, index1) => {
     star.addEventListener("click", () => {
         stars.forEach((star, index2) => {
-            atual = index1 + 1;
             index1 >= index2 ? star.classList.add('active') : star.classList.remove("active");
         })
+        atual = index1 + 1;
+        localStorage.setItem('estrelas-sportv', atual);
     })
 });
